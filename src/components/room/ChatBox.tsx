d9a6da73210be4d9191b48899273cfdcf689a376
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-03 13:29:22 +0900

    feat : use icon

--- src/components/room/ChatBox.tsx
+++ src/components/room/ChatBox.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 function ChatBox() {
 
@@ -6,7 +9,10 @@ function ChatBox() {
     <div className="w-full h-[558px] rounded-lg border flex flex-col justify-between">
       <div className="border-b h-12 flex items-center px-4 justify-between">
         <div className="font-bold">채팅</div>
-        <div className="text-sm hover:bg-gray-100 p-1.5 rounded-lg">참여자 29</div>
+        <div className="flex gap-1 items-center text-sm hover:bg-gray-100 p-1.5 rounded-lg">
+          <FontAwesomeIcon icon={faUserAlt} />
+          <div>29</div>
+        </div>
       </div>
       <div>
         <div className="border-b text-sm pb-5 px-4 flex flex-col justify-end gap-2 overflow-y-scroll h-[446px] scrollbar-hide">
@@ -20,7 +26,10 @@ function ChatBox() {
         ))}
         </div>
         <div className="h-16 px-4 flex items-center text-sm">
-          <input className="bg-gray-100 h-10 w-full rounded-lg focus:outline-none px-2" placeholder="채팅 입력"></input>
+          <div className="bg-gray-100 h-10 w-full rounded-lg flex items-center justify-center gap-2">
+            <input className="w-10/12 bg-transparent focus:outline-none" placeholder="채팅 입력"></input>
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/room/DrawingTool.tsx
+++ src/components/room/DrawingTool.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faEraser, faFill, faPencil } from "@fortawesome/free-solid-svg-icons";
+
 
 function DrawingTool() {
-
+  const icons = [
+    { icon: faPencil, label: "Pencil" },
+    { icon: faEraser, label: "Eraser" },
+    { icon: faFill, label: "Fill" },
+    { icon: faArrowLeft, label: "Undo" },
+    { icon: faArrowRight, label: "Redo" },
+  ];
+  
   return (  
-    <div className="rounded-xl border grid grid-cols-[24px_240px_24px_1fr_24px] xl:grid-cols-[24px_327px_24px_1fr_24px] 2xl:grid-cols-[24px_420px_24px_1fr_24px] items-center py-4 text-xs lg:text-sm">
+    <div className="rounded-xl border grid grid-cols-[24px_212px_24px_1fr_24px] xl:grid-cols-[24px_290px_24px_1fr_24px] 2xl:grid-cols-[24px_420px_24px_1fr_24px] items-center py-4 text-xs lg:text-sm">
       <div></div>
       <div className="flex flex-col gap-2">
         <div>색상</div>
-        <div className="rounded-xl grid grid-cols-6 gap-2 xl:gap-3 2xl:gap-1 xl:grid-cols-8 2xl:grid-cols-12">
+        <div className="rounded-xl grid grid-cols-6 gap-1 xl:grid-cols-8 2xl:grid-cols-12">
           {Array.from({ length: 24 }).map((_, index) => (
             <div key={index} className="w-8 h-8 bg-gray-200 rounded"></div>
           ))}
@@ -15,13 +25,15 @@ function DrawingTool() {
       </div>
       <div></div>
       <div className="grid grid-rows-[1fr_8px_2fr] lg:grid-rows-[1fr_16px_1fr] h-full xl:grid-rows-[1fr_8px_1fr] 2xl:grid-cols-[1fr_24px_2fr] 2xl:grid-rows-1">
-        <div className="grid grid-cols-[1fr_200px_1fr] lg:grid-cols-[1fr_334px_1fr] xl:grid-cols-[1fr_280px_1fr]">
+        <div className="grid grid-cols-[1fr_200px_1fr] lg:grid-cols-[1fr_280px_1fr] xl:grid-cols-[1fr_200px_1fr] 2xl:grid-cols-[1fr_280px_1fr]">
           <div></div>
           <div className="flex flex-col gap-2">
             <div>도구</div>
-            <div className="h-full grid grid-cols-5 gap-2 lg:gap-4 2xl:gap-2">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <div key={index} className="bg-gray-100 rounded 2xl:h-12 hover:bg-gray-200"></div>
+            <div className="h-full grid grid-cols-5 gap-3.5 lg:gap-2 2xl:items-center">
+              {icons.map(({ icon, label }, index) => (
+                <div key={index} className="bg-gray-100 rounded 2xl:h-12 hover:bg-gray-200 flex items-center justify-center" title={label}>
+                  <FontAwesomeIcon icon={icon} size="lg" />
+                </div>
               ))}
             </div>
           </div>
@@ -31,11 +43,15 @@ function DrawingTool() {
         <div className="items-center w-full h-full flex flex-col lg:flex-row gap-2 lg:gap-4 2xl:gap-6">
           <div className="w-[200px] lg:w-full h-full flex flex-col gap-2">
             <div>굵기</div>
-            <div className="h-full bg-gray-100 rounded 2xl:h-12"></div>
+            <div className="h-full grid items-center">
+              <div className="h-full bg-gray-100 rounded 2xl:h-12"></div>
+            </div>
           </div>
           <div className="w-[200px] lg:w-full h-full flex flex-col gap-2">
             <div>투명도</div>
-            <div className="h-full bg-gray-100 rounded 2xl:h-12"></div>
+            <div className="h-full grid items-center">
+              <div className="h-full bg-gray-100 rounded 2xl:h-12"></div>
+            </div>
           </div>
         </div>
       </div>
