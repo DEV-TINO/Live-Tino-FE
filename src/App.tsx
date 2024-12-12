2cc2a2ee8bb544f61a2be7794db0c9c3b062eefe
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-13 01:45:13 +0900

    feat : install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./index.css";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./components/main/MainPage";
 import LiveRoomPage from "./components/room/LiveRoomPage";
 
 function App() {
   return (
-    <LiveRoomPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="live" element={<LiveRoomPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
