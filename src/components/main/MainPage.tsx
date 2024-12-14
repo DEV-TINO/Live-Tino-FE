747fa66735aea1f6e0bccd72070f996d65c8d94f
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-15 05:01:01 +0900

    feat : add timer layout in main page

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import RoomButton from "./RoomButton"
+import TimerTab from "./TimerTab";
 
 function MainPage() {
 
   return (
-    <div className="">
-      <RoomButton label="방 생성"/>
-      <RoomButton label="입장"/>
+    <div className="grid grid-cols-[minmax(24px,_1fr)_minmax(400px,600px)_minmax(24px,_1fr)]">
+      <div></div>
+      <div className="flex flex-col gap-6 pt-32">
+        <TimerTab />
+        <div className="flex gap-6">
+          <RoomButton label="방 생성"/>
+          <RoomButton label="입장"/>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
