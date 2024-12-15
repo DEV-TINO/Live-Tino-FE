dd7208f7a18515dea7555eced265aa836a53732d
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-15 16:43:38 +0900

    feat : add buttons in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,6 +15,8 @@ function Header() {
     <div className="flex justify-between items-center px-10 w-full h-16">
       <button onClick={handleClickLogo} className="text-3xl font-semibold text-blue-600">LIVE-TINO</button>
       <div className="flex gap-6">
+        <button>방송설정</button>
+        <button>방송종료</button>
         <button onClick={handleClickMyPage}>마이페이지</button>
         <button>로그인</button>
       </div>
