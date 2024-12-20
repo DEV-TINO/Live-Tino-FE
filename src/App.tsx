c382e9c31ce0b82adc4bbc4e52632413290dfa30
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-21 03:20:43 +0900

    feat : remove header in register and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import "./index.css";
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./components/main/MainPage";
 import LiveRoomPage from "./components/room/LiveRoomPage";
 import MyPage from "./components/mypage/MyPage";
 import Header from "./components/Header";
 import PlayPage from "./components/mypage/PlayPage";
+import LoginPage from "./components/login/LoginPage";
+import RegisterPage from "./components/login/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="pb-4">
-        <Header />
-      </div>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/live" element={<LiveRoomPage />} />
-        <Route path="/my" element={<MyPage />} />
-        <Route path="/video" element={<PlayPage />} />
-      </Routes>
+      <AppContent />
     </BrowserRouter>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <div>
+      {location.pathname !== "/login" && location.pathname !== "/register" && <Header />}
+      <div className="pb-4">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/live" element={<LiveRoomPage />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/video" element={<PlayPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
