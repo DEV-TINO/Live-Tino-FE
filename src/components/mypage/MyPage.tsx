38ac1b8dd8424df4f8792bd2ee089af52db83a90
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-21 03:22:35 +0900

    style : change search bar css style

--- src/components/mypage/MyPage.tsx
+++ src/components/mypage/MyPage.tsx
@@ -10,7 +10,7 @@ function MyPage() {
       <div className="col-start-2 row-start-2">
         <ProfileTab />
       </div>
-      <form className="col-start-4 w-64 ml-auto mb-4">   
+      <form className="col-start-4 w-72 ml-auto mb-4">   
         <label className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -19,7 +19,7 @@ function MyPage() {
             </svg>
           </div>
           <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="제목" required />
-          <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">검색</button>
+          <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2">검색</button>
         </div>
       </form>
       <div className="col-start-4 row-start-2 flex flex-col gap-4">
