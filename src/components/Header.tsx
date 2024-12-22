ed1a98969fea4b1df7e40943ffc8586febced2c8
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-22 14:29:14 +0900

    feat : hide mypage in liveroom screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isLogin: boolean = false;
 
   const handleClickLogo = () => {
     navigate(`/`);
@@ -20,10 +22,23 @@ function Header() {
     <div className="flex justify-between items-center px-10 w-full h-16">
       <button onClick={handleClickLogo} className="text-3xl font-semibold text-blue-600">LIVE-TINO</button>
       <div className="flex gap-6">
-        <button>방송설정</button>
-        <button>방송종료</button>
-        <button onClick={handleClickMyPage}>마이페이지</button>
-        <button onClick={handleClickLoginPage}>로그인</button>
+        {location.pathname === "/live" && (
+          <div className="flex gap-6">
+            <button>방송설정</button>
+            <button>방송종료</button>
+            </div>
+        )}
+        {location.pathname !== "/live" && (
+          <button onClick={handleClickMyPage}>마이페이지</button>
+        )}
+        {isLogin ? (
+          <div className="flex gap-1">
+            <div className="font-semibold">김태건</div>
+            <div>님</div>
+          </div>
+        ) : (
+          <button onClick={handleClickLoginPage}>로그인</button>
+        )}
       </div>
     </div>
   );
