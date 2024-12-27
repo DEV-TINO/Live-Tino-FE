8be9de995acb162e659229cf40b9270756e432aa
jzongyun <jeong.yun@tukorea.ac.kr>
2024-12-27 22:38:30 +0900

    style : change button names to english

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,16 +20,16 @@ function Header() {
 
   return (
     <div className="flex justify-between items-center px-10 w-full h-16">
-      <button onClick={handleClickLogo} className="text-3xl font-semibold text-blue-600">LIVE-TINO</button>
+      <button onClick={handleClickLogo} className="text-3xl font-semibold text-blue-600 min-w-40 text-start">LIVE-TINO</button>
       <div className="flex gap-6">
         {location.pathname === "/live" && (
           <div className="flex gap-6">
-            <button>방송설정</button>
-            <button>방송종료</button>
+            <button className="min-w-14">Setting</button>
+            <button className="min-w-7">Exit</button>
             </div>
         )}
         {location.pathname !== "/live" && (
-          <button onClick={handleClickMyPage}>마이페이지</button>
+          <button className="min-w-16 text-end" onClick={handleClickMyPage}>MyPage</button>
         )}
         {isLogin ? (
           <div className="flex gap-1">
@@ -37,7 +37,7 @@ function Header() {
             <div>님</div>
           </div>
         ) : (
-          <button onClick={handleClickLoginPage}>로그인</button>
+          <button className="min-w-10 text-end" onClick={handleClickLoginPage}>Login</button>
         )}
       </div>
     </div>
