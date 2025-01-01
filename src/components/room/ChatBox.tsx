4b84fd118197d9b979b5ba574f13639d3a957bfd
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-01 14:48:45 +0900

    feat : make liveroom page

---
+++ src/components/room/ChatBox.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+
+function ChatBox() {
+
+  return (  
+    <div className="w-full h-full rounded-md border flex flex-col justify-between">
+      <div className="border-b flex items-center h-12 px-4 justify-between">
+        <div className="font-bold">Chatting</div>
+        <div className="flex gap-1 items-center text-sm hover:bg-gray-100 p-1.5 rounded-md">
+          <FontAwesomeIcon icon={faUserAlt} />
+          <div>29</div>
+        </div>
+      </div>
+      <div className="overflow-y-auto h-[395px] xl:h-[475px] 2xl:h-[577px] scrollbar-hide">
+        <div className="text-sm py-5 px-4 flex flex-col justify-end gap-2">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div key={index} className="flex gap-2">
+            <div className="text-red-500 font-bold">hmyang</div>
+            <div>
+              안녕하세요 반갑습니다 안녕하세요 반갑습니다 안녕하세요 반갑습니다 안녕하세요 반갑습니다
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
+      <div className="border-t h-16 px-4 flex items-center text-sm">
+        <div className="bg-gray-100 h-10 w-full rounded-md flex items-center justify-center gap-2">
+          <input className="w-10/12 bg-transparent focus:outline-none" placeholder="Enter Chat"></input>
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChatBox;
---
+++ src/components/room/DrawingTool.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function DrawingTool() {
+  return (  
+    <div className="rounded-md border grid grid-cols-[24px_220px_24px_1fr_24px] xl:grid-cols-[12px_340px_12px_1fr_12px] 2xl:grid-cols-[24px_340px_24px_1fr_24px] items-center py-4">
+      <div></div>
+      <div className="flex flex-col gap-2">
+        <div>Color</div>
+        <div className="rounded-md grid grid-cols-6 gap-1 xl:grid-cols-12">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div key={index} className="h-8 w-8 xl:w-6 xl:h-6 bg-gray-200 rounded"></div>
+          ))}
+        </div>
+      </div>
+      <div></div>
+      <div className="flex flex-col xl:flex-row gap-2 xl:gap-3 2xl:gap-6 w-full justify-center items-center">
+        <div className="flex flex-col gap-2 w-full max-w-64">
+          <div>Thickness</div>
+          <div className="bg-gray-100 rounded w-full h-[50px] xl:h-[52px] flex justify-between p-4 items-center">
+            {Array.from({ length: 5 }).map((_, index) => {
+              const size = (index + 1) * 4;
+              return (
+                <div key={index} className="w-8 h-8 border border-gray-300 hover:border-gray-400 rounded-full flex items-center justify-center">
+                  <div className="bg-gray-300 rounded-full" style={{ width: size, height: size }}></div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 w-full max-w-64">
+          <div>Tranparency</div>
+          <div className="bg-gray-100 rounded w-full h-[50px] xl:h-[52px] flex items-center gap-2 p-4 justify-center">
+            <div className="min-w-8 h-8 border border-gray-300 rounded-full"></div>
+            <input type="range" min="0" max="100" className="w-full h-1.5 appearance-none bg-gray-300 rounded-full accent-gray-400"></input>
+            <div className="min-w-8 h-8 border border-gray-300 rounded-full bg-gray-300"></div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default DrawingTool;
