936e2b7e04738f606c788cb149f06db088dc1d90
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-02 15:55:43 +0900

    style : change live room page setting modal css style

--- src/components/room/SettingModal.tsx
+++ src/components/room/SettingModal.tsx
@@ -12,9 +12,9 @@ function SettingModal() {
 
   return (
     <div>
-      <div onClick={handleModalClick} className="relative p-4 w-96 max-w-md max-h-full">
+      <div onClick={handleModalClick} className="relative p-4 w-[400px] max-w-md max-h-full">
         <div className="relative bg-white rounded-md shadow">
-          <div className="flex items-center justify-between p-4 border-b rounded-t">
+          <div className="flex items-center justify-between px-4 py-2 border-b rounded-t">
             <h3 className="text-lg font-semibold text-gray-900">Setting</h3>
             <button onClick={closeModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-md text-sm w-8 h-8 inline-flex justify-center items-center">
               <svg
@@ -36,34 +36,38 @@ function SettingModal() {
             </button>
           </div>
           <form className="p-4">
-            <div className="grid gap-4 mb-4 grid-cols-2">
-              <div className="">
+            <div className="grid gap-4 mb-4 grid-cols-2 px-2">
+              <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900">
                   방 설정
                 </label>
-                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5">
-                  <option selected>선택</option>
-                  <option value="public">public</option>
-                  <option value="private">private</option>
-                </select>
+                <div className="bg-gray-50 outline outline-1 outline-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 focus-within:outline-blue-600 focus-within:outline-2">
+                  <select className="bg-transparent w-full focus:outline-none">
+                    <option selected>선택</option>
+                    <option value="public">public</option>
+                    <option value="private">private</option>
+                  </select>
+                </div>
               </div>
-              <div className="">
+              <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900">
                   화면 설정
                 </label>
-                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5">
-                  <option selected>선택</option>
-                  <option value="camera">camera</option>
-                  <option value="screen">screen share</option>
-                  <option value="board">whiteboard</option>
-                </select>
+                <div className="bg-gray-50 outline outline-1 outline-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 focus-within:outline-blue-600 focus-within:outline-2">
+                  <select className="bg-transparent w-full focus:outline-none">
+                    <option selected>선택</option>
+                    <option value="camera">camera</option>
+                    <option value="screen">screen share</option>
+                    <option value="board">whiteboard</option>
+                  </select>
+                </div>
               </div>
               <div className="col-span-2">
                 <label className="block mb-2 text-sm font-medium text-gray-900">
                   공유
                 </label>
                 <div className="flex gap-2">
-                  <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5" placeholder="https://live-tino.com/12" />
+                  <div className="bg-gray-50 border border-gray-300 text-gray-400 text-sm rounded-md block w-full p-2.5">https://live-tino.com/12</div>
                   <button className="text-sm min-w-14 p-2.5 rounded-md border">공유</button>
                 </div>
               </div>
