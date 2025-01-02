8eec2f51e7c982c8fb709e9eb8de9c11f20c1ef5
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-02 21:51:34 +0900

    feat : create viewer modal in liveroom page

--- src/components/room/ChatBox.tsx
+++ src/components/room/ChatBox.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import useViewerModal from "../../stores/viewerModal";
+import ViewerModal from "./ViewerModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 function ChatBox() {
+  const { openModal, isModalOpen } = useViewerModal();
 
   return (  
-    <div className="w-full h-full rounded-md border flex flex-col justify-between">
+    <div className="relative w-full h-full rounded-md border flex flex-col justify-between">
+      {isModalOpen && <ViewerModal />}
       <div className="border-b flex items-center h-12 px-4 justify-between">
         <div className="font-bold">Chatting</div>
-        <div className="flex gap-1 items-center text-sm hover:bg-gray-100 p-1.5 rounded-md">
+        <button onClick={openModal} className="flex gap-1 items-center text-sm hover:bg-gray-100 p-1.5 rounded-md">
           <FontAwesomeIcon icon={faUserAlt} />
           <div>29</div>
-        </div>
+        </button>
       </div>
       <div className="overflow-y-auto h-[395px] xl:h-[475px] 2xl:h-[577px] scrollbar-hide">
         <div className="text-sm py-5 px-4 flex flex-col justify-end gap-2">
         {Array.from({ length: 12 }).map((_, index) => (
           <div key={index} className="flex gap-2">
-            <div className="text-red-500 font-bold">hmyang</div>
+            <div className="text-blue-600 font-bold">hmyang</div>
             <div>
               안녕하세요 반갑습니다 안녕하세요 반갑습니다 안녕하세요 반갑습니다 안녕하세요 반갑습니다
             </div>
---
+++ src/components/room/ViewerModal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import useViewerModal from "../../stores/viewerModal";
+
+function ViewerModal() {
+  const { closeModal } = useViewerModal();
+
+  const handleModalClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div onClick={handleModalClick} className="absolute top-9 left-0 right-0 bottom-0 flex justify-center z-50 h-96 px-4">
+      <div className="border relative p-4 w-full max-w-sm max-h-full bg-white rounded-md shadow-xl">
+        <button onClick={closeModal} className="absolute top-3 right-3 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-md text-sm w-8 h-8 inline-flex justify-center items-center">
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close modal</span>
+        </button>
+        <div className="font-bold pb-2">Viewer</div>
+        <div className="flex flex-col gap-2 overflow-y-auto h-80 scrollbar-hide pb-2">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div key={index} className="flex gap-1 items-end">
+            <div className="text-blue-600">hmyang</div>
+            <div className="text-sm text-gray-400">(hmyang0329)</div>
+          </div>
+        ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default ViewerModal;
