74c197c81b185aa5750d17faecbfee173ed799e7
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-04 04:48:25 +0900

    style : change play page css style

--- src/components/mypage/PlayPage.tsx
+++ src/components/mypage/PlayPage.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import VideoArea from "./VideoArea";
 import PlayVideoTable from "./PlayVideoTable"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import Pagination from "./Pagination";
 
 function PlayPage() {
@@ -10,11 +8,24 @@ function PlayPage() {
   return (
     <div className="grid grid-cols-[minmax(24px,_1fr)_minmax(512px,992px)_24px_320px_minmax(24px,_1fr)]">
       <div></div>
-      <div>
+      <div className="flex flex-col gap-2">
         <VideoArea />
-        <div className="text-2xl font-semibold py-4 pr-2 flex justify-between">
+        <div className="text-2xl font-semibold py-4 pr-2 flex flex-col gap-1">
           <div>그림 맞추기 하자</div>
-          <FontAwesomeIcon icon={faFloppyDisk} />
+          <div className="flex gap-4 text-gray-600">
+            <div className="text-base flex gap-2">
+              <div>방송 일자</div>
+              <div className="font-normal">2024.3.22</div>
+            </div>
+            <div className="text-base flex gap-2">
+              <div>영상 길이</div>
+              <div className="font-normal">00:25:38</div>
+            </div>
+            <div className="text-base flex gap-2">
+              <div>저장 횟수</div>
+              <div className="font-normal">3회</div>
+            </div>
+          </div>
         </div>
       </div>
       <div></div>
--- src/components/mypage/VideoArea.tsx
+++ src/components/mypage/VideoArea.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical, faExpand, faPlay, faVolumeLow } from "@fortawesome/free-solid-svg-icons";
 
 function VideoArea() {
+  const [value, setValue] = useState(0);
 
-  return (  
-    <div className="bg-gray-200 aspect-[16/9]"></div>
+  return (
+    <div className="relative group">
+      <div className="flex text-white justify-between text-lg items-center opacity-0 group-hover:opacity-60 absolute top-0 left-0 w-full h-12 bg-gradient-to-b from-neutral-950 to-transparent px-4">
+        <div className="font-semibold">그림 맞추기 하자</div>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+      </div>
+      <div className="flex flex-col justify-between opacity-0 group-hover:opacity-60 absolute bottom-0 left-0 w-full h-14 bg-gradient-to-b from-transparent to-neutral-950 px-2">
+      <div className="relative w-full h-1 bg-gray-300 rounded-full">
+        <div className="absolute h-1 bg-red-500 rounded-full" style={{ width: `${value}%` }}></div>
+        <input type="range" min="0" max="100" value={value} className="absolute w-full h-1 opacity-0 cursor-pointer" onInput={(e) => setValue(Number((e.target as HTMLInputElement).value))} />
+        <div className="absolute top-[-4px] w-3 h-3 bg-red-500 rounded-full" style={{ left: `${value}%` }}></div>
+      </div>
+        <div className="flex text-white justify-between items-center w-full px-4 pb-5">
+          <div className="flex gap-4 items-center">
+            <FontAwesomeIcon icon={faPlay} />
+            <FontAwesomeIcon icon={faVolumeLow} />
+            <div className="text-sm">00:00:00 / 00:25:38</div>
+          </div>
+          <FontAwesomeIcon icon={faExpand} />
+        </div>
+      </div>
+      <div className="bg-gray-200 aspect-[16/9]"></div>
+    </div>
   );
 }
 
