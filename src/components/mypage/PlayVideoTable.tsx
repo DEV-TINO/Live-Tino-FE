d3df4e4143a14f6697b243b5a9bb8e77cfba7b9d
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-16 22:40:04 +0900

    feat : move to play page when clicl play video table

--- src/components/mypage/PlayVideoTable.tsx
+++ src/components/mypage/PlayVideoTable.tsx
@@ -1,17 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import useVideoStore from "../../stores/videoStore";
+import { useNavigate } from "react-router-dom";
 
-const PlayVideoTable = ({ itemsPerPage }: { itemsPerPage: number }) => {
-  const { videos, currentPage } = useVideoStore();
+const PlayVideoTable = ({ videoId }: { videoId: number }) => {
+  const { videos, currentPage, itemsPerPage } = useVideoStore();
+  const navigate = useNavigate();
   const startIndex = (currentPage - 1) * itemsPerPage;
+
   const currentVideos = videos.slice(startIndex, startIndex + itemsPerPage);
+
+  const handleClickVideo = (videoId: number) => {
+    navigate(`/video/${videoId}`);
+  };
 
   return (
     <div className="flex flex-col gap-3 w-full">
-      {currentVideos.map((video, index) => (
-        <div key={index} className="flex w-full">
-          <div className="relative min-w-36 aspect-[16/9] bg-gray-200 rounded-md cursor-pointer">
+      {currentVideos.map((video) => (
+        <div
+          key={video.id}
+          onClick={() => handleClickVideo(video.id)}
+          className="flex w-full cursor-pointer"
+        >
+          <div className="relative min-w-36 aspect-[16/9] bg-gray-200 rounded-md">
             <div className="absolute bottom-2 right-2 bg-neutral-950 bg-opacity-50 text-white text-xs px-1 py-0.5 rounded">
               {video.duration}
             </div>
