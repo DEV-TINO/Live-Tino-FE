71a5e85e169362b3a18884d8af77605e9b7c4032
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-25 09:09:27 +0900

    feat : add search bar above the video list in play page

--- src/components/mypage/PlayPage.tsx
+++ src/components/mypage/PlayPage.tsx
@@ -33,7 +33,29 @@ const PlayPage = () => {
         </div>
       </div>
       <div></div>
-      <div className="flex flex-col items-center h-[620px] justify-between">
+      <div className="flex flex-col items-center h-[672px] justify-between">
+        <form className="col-start-4 w-full ml-auto">
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+            </div>
+            <input 
+              type="search" 
+              id="default-search" 
+              className="block w-full p-3 ps-10 h-11 text-sm text-gray-900 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" 
+              placeholder="제목" 
+              required 
+            />
+            <button 
+              type="submit" 
+              className="text-white absolute end-1.5 bottom-1.5 bg-blue-600 hover:bg-blue-800 focus:outline-none font-medium rounded-md text-sm px-4 py-1.5"
+            >
+              검색
+            </button>
+          </div>
+        </form>
         <PlayVideoTable videoId={video.id} />
         <Pagination />
       </div>
