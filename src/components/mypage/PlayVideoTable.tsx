5c7e809da0ab3ee6d4694876f5fc183c9ca60e6e
jzongyun <jeong.yun@tukorea.ac.kr>
2025-02-16 16:20:22 +0900

    feat : integrate API for fetching video data

--- src/components/mypage/PlayVideoTable.tsx
+++ src/components/mypage/PlayVideoTable.tsx
@@ -1,32 +1,38 @@
+import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import useVideoStore from "../../stores/videoStore";
 import { useNavigate } from "react-router-dom";
 import useBaseModal from "../../stores/baseModal";
 import SelectionModal from "./SelectionModal";
+import useUserStore from "../../stores/userStore";
 
-const PlayVideoTable = ({ videoId }: { videoId: number }) => {
-  const { videos, currentPage, itemsPerPage, searchQuery, selectedVideoId, setSelectedVideoId } = useVideoStore();
+const PlayVideoTable = ({ videoId }: { videoId: string }) => {
+  const { videos, currentPage, itemsPerPage, searchQuery, selectedVideoId, setSelectedVideoId, fetchVideos } = useVideoStore();
   const { openModal, isModalOpen, modalType } = useBaseModal();
+  const { userId } = useUserStore();
   const navigate = useNavigate();
-  const startIndex = (currentPage - 1) * itemsPerPage;
+  
+  useEffect(() => {
+    fetchVideos(userId);
+  }, []);
 
+  const startIndex = (currentPage - 1) * itemsPerPage;
   const filteredVideos = videos.filter((video) => video.title.includes(searchQuery));
   const currentVideos = filteredVideos.slice(startIndex, startIndex + itemsPerPage);
 
-  const handleClickVideo = (videoId: number) => {
+  const handleClickVideo = (videoId: string) => {
     navigate(`/video/${videoId}`);
   };
 
-  const handleClickSelection = (event: React.MouseEvent, id: number) => {
+  const handleClickSelection = (event: React.MouseEvent, id: string) => {
     setSelectedVideoId(id);
     event.stopPropagation();
     openModal("selection");
   };
 
-  const getVideoTitleClass = (id: number) => {
-    if (id === videoId) return "text-blue-600 font-semibold";
-    return "text-black";
+  const getVideoTitleClass = (id: string) => {
+    return id === videoId ? "text-blue-600 font-semibold" : "text-black";
   };
 
   return (
@@ -38,6 +44,11 @@ const PlayVideoTable = ({ videoId }: { videoId: number }) => {
           className="flex w-full cursor-pointer"
         >
           <div className="relative min-w-36 aspect-[16/9] bg-gray-200 rounded-md">
+            {video.thumbnail ? (
+              <img src={video.thumbnail} alt={video.title} className="w-full h-full object-cover rounded-md" />
+            ) : (
+              <div className="w-full h-full flex items-center justify-center text-gray-500">No Thumbnail</div>
+            )}
             <div className="absolute bottom-2 right-2 bg-neutral-950 bg-opacity-50 text-white text-xs px-1 py-0.5 rounded">
               {video.duration}
             </div>
