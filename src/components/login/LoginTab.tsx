fe4f3bdf7819bc9c0e45c50ef62368b519686f5f
jzongyun <jeong.yun@tukorea.ac.kr>
2025-03-02 07:29:52 +0900

    feat: link api login

--- src/components/login/LoginTab.tsx
+++ src/components/login/LoginTab.tsx
@@ -1,28 +1,50 @@
 import { useNavigate } from "react-router-dom";
+import useUserStore from "../../stores/userStore";
 
 const LoginTab = () => {
   const navigate = useNavigate();
+  const { login } = useUserStore();
 
   const handleClickPassword = () => {
     navigate("/password");
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const loginId = formData.get("loginId") as string;
+    const password = formData.get("password") as string;
+
+    const success = await login(loginId, password);
+
+    if (success) {
+      navigate("/");
+    } else {
+      alert("Fail to log in");
+    }
+  };
+
   return (
-    <form className="w-full flex flex-col gap-4">
-      <input 
-        type="text" 
-        id="id" 
-        className="border border-gray-300 text-gray-900 text-sm rounded-md focus:border-blue-500 block w-full p-2.5" 
-        placeholder="Username" 
-        required 
-      />
-      <input 
-        type="password" 
-        id="password" 
-        className="border border-gray-300 text-gray-900 text-sm rounded-md focus:border-blue-500 block w-full p-2.5" 
-        placeholder="Password" 
-        required 
-      />
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <div>
+        <input 
+          type="text" 
+          name="loginId" 
+          className="border border-gray-300 rounded-md w-full p-2.5 text-sm focus:border-blue-500" 
+          placeholder="Enter username" 
+          required 
+        />
+      </div>
+      <div>
+        <input 
+          type="password" 
+          name="password" 
+          className="border border-gray-300 rounded-md w-full p-2.5 text-sm focus:border-blue-500" 
+          placeholder="Enter password" 
+          required 
+        />
+      </div>
       <div 
         onClick={handleClickPassword} 
         className="text-gray-600 text-right text-sm cursor-pointer"
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,21 +1,45 @@
-import { create } from "zustand"; 
-
-const USER_ID = "1";
+import { create } from "zustand";
+import axios from "axios";
 
 interface IUserStore {
   userId: string;
+  setUserId: (userId: string) => void;
   nickname: string;
   setNickname: (nickname: string) => void;
   id: string;
   setId: (id: string) => void;
+  login: (loginId: string, password: string) => Promise<boolean>;
+  isLogin: boolean;
 }
 
 const useUserStore = create<IUserStore>((set) => ({
-  userId: USER_ID,
-  nickname: "김태건",
+  userId: "",
+  setUserId: (userId) => set({ userId }),
+  nickname: "",
   setNickname: (nickname) => set({ nickname }),
-  id: "idydy",
+  id: "",
   setId: (id) => set({id}),
+  isLogin: false,
+  login: async (loginId, password) => {
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/user/login`, {
+        loginId,
+        userPassword: password,
+      });
+
+      const userData = response.data;
+
+      if (userData.success) {
+        set({ id: loginId, nickname: userData.userInfo.userName, userId: userData.userInfo.userId, isLogin: true });
+
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error("Login Error:", error);
+      return false;
+    }
+  },
 }));
 
 export default useUserStore;
